8cd9a6eb8b2bf12f6e7b1d10947efa40e339b57e
Vimal mishra <vm45231@gmail.com>
2025-12-29 12:22:43 +0530

    cookiefixed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -148,8 +148,8 @@ const token =  jwtSign(user._id)
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false, // true if using HTTPS
-    sameSite: "lax",
+    secure: true, // true if using HTTPS
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
